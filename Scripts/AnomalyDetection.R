6f277a5b07e5f0f624bfea19d1630926384229a1
OakAlice <oakleigh.wilson05@gmail.com>
2024-06-19 15:32:23 +1000

    Continued anomaly detection

--- Scripts/AnomalyDetection.R
+++ Scripts/AnomalyDetection.R
@@ -1,9 +1,5 @@
 # Anomaly Detection
 
-# set up ####
-library(pacman)
-p_load(tidyverse, data.table, e1071)
-
 # functions ####
 # function for splitting the data
 split_data <- function(data, targetActivity) {
@@ -48,12 +44,12 @@ unsup_1_class_SVM_results <- function(
   split <- split_data(fullData, targetActivity) 
   
   # process the training and validation data
-  training_data <- process_data(split$training, features_list, window_length = window_length, 
+  training_data <- process_data(na.omit(split$training), features_list, window_length = window_length, 
                                 overlap_percent = overlap_percent, down_Hz = down_Hz, 
                                 feature_normalisation = feature_normalisation)
   training_data <- training_data %>% select(-c(ID))
   
-  validation_data <- process_data(split$validation, features_list, window_length = window_length, 
+  validation_data <- process_data(na.omit(split$validation), features_list, window_length = window_length, 
                                   overlap_percent = overlap_percent, down_Hz = down_Hz, 
                                   feature_normalisation = feature_normalisation)
   validation_data <- validation_data %>% select(-c(ID))
@@ -75,13 +71,19 @@ unsup_1_class_SVM_results <- function(
   # make predictions with the trained SVM onto predictor only data for training
   predicted_training <- predict(unsup_1_class_SVM, training_data_predictors) # predict on training data
   predicted_training <- as.factor(ifelse(predicted_training == "TRUE", "Normal", "Outlier"))
-  reference_training <- as.factor(ifelse(training_data_labels$Activity == "Locomotion", "Normal", "Outlier"))
+  reference_training <- as.factor(ifelse(training_data_labels$Activity == targetActivity, "Normal", "Outlier"))
   training_performance <- table(Predicted = predicted_training, Reference = reference_training)
   
   # do the same for validation
   predicted_validation <- predict(unsup_1_class_SVM, validation_data_predictors) # predict on training data
+  
+  # lengths
+  length(validation_data_predictors$mean_Accel_X)
+  length(predicted_validation)
+  
+  predicted_validation <- predict(unsup_1_class_SVM, validation_data_predictors) # predict on validation data
   predicted_validation <- as.factor(ifelse(predicted_validation == "TRUE", "Normal", "Outlier"))
-  reference_validation <- as.factor(ifelse(validation_data_labels$Activity == "Locomotion", "Normal", "Outlier")) 
+  reference_validation <- as.factor(ifelse(validation_data_labels$Activity == targetActivity, "Normal", "Outlier")) 
   validation_performance <- table(Predicted = predicted_validation, Reference = reference_validation)
   
   return(list(training_performance = training_performance,
@@ -109,24 +111,24 @@ flatten_confusion_matrix <- function(conf_matrix, prefix) {
 
 
 
+# Run Script ####
+library(pacman)
+p_load(tidyverse, data.table, stringr, tools, e1071)
 
-
+base_path <- "R:/FSHEE/Science/Unsupervised-Accel/Other_Projects/Jordan_Perentie/PerentieAnalysis"
   
-# run script ####
-  base_path <- "R:/FSHEE/Science/Unsupervised-Accel/Other_Projects/Jordan_Perentie/PerentieAnalysis"
-  
-  labelled_data <- fread(file.path(base_path, "Controlled_DiCicco_Perentie_Labelled.csv"))
-  tstDat <- labelled_data %>% filter(ID == "Abigail") # individual not included in training
-  otherDat <- labelled_data %>% filter(!ID == "Abigail") # other data
+labelled_data <- fread(file.path(base_path, "Controlled_DiCicco_Perentie_Labelled.csv"))
+tstDat <- labelled_data %>% filter(ID == "Abigail") # individual not included in training
+otherDat <- labelled_data %>% filter(!ID == "Abigail") # other data
 
 # define variables
-  targetActivity_options <- c("Locomotion", "Inactive")
+  targetActivity_options <- c("Locomotion")
   window_length_options <- c(0.5, 1, 2)
   overlap_percent_options <- c(0, 10, 50)
   down_Hz_options <- 50
   feature_normalisation_options <- c("MinMaxScaling", "Standardisation")
   nu_options <- c(0.01, 0.05, 0.10, 0.15, 0.20)
-  kernel_options <- c("linear", "polynomial", "radial", "sigmoid")
+  kernel_options <- c("polynomial", "radial", "sigmoid")
   features_list <- c("mean", "max", "min", "sd", "cor", "SMA", "minODBA", "maxODBA", "minVDBA", "maxVDBA", 
                      "RMS", "FFT", "entropy", "zero_crossing")
   
@@ -151,7 +153,6 @@ results_sheet <- lapply(1:nrow(options_df), function(i) {
               nu = options_df[i, "nu"],
               kernel = options_df[i, "kernel"]
       )
-    results[[paste0("iteration_", i)]] <- result
   }, error = function(e) {
     # Print error message and skip iteration
     message(paste("Error in iteration", i, ":", conditionMessage(e)))
